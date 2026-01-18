29737a0b660505f6111a7960475acafdc48f0856
Chrysanthi Matsagka <131909303+cmatsagka@users.noreply.github.com>
2026-01-18 17:27:41 +0200

    Populate script with books and create function to display library

--- script.js
+++ script.js
@@ -15,3 +15,16 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
+addBookToLibrary('Other Minds', 'Godfrey Smith', '255', 'have read');
+addBookToLibrary('Project Hail Mary', 'Andy Weir', '473', 'have read');
+addBookToLibrary('The Great Gatsby', 'F. Scott Fitzgerald', '180', 'not read yet');
+addBookToLibrary('Thinking, Fast and Slow', 'Daniel Kahneman', '499', 'have read');
+addBookToLibrary('Atomic Habits', 'James Clear', '320', 'not read yet');
+
+function displayLibrary(library) {
+    library.forEach(book => {
+        console.log(book);
+    });
+}
+
+displayLibrary(myLibrary);
